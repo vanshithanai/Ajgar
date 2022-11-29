ed1a0dab99740d07613962342d50bcaf49c8d547
vanshithanai <vanshithanai26@gmail.com>
2022-11-30 03:06:58 +0530

    Actually ToDo for encoder and decoder done. Dict in Json file. Added Homework file for practice questions.

--- Encoder_Decoder.py
+++ Encoder_Decoder.py
@@ -1,52 +1,61 @@
+import json
+
 class EncoderDecoder(object):
 
     def __init__(self):
-        self.encodeDict = {"A" : "p", 
-                "B" : "o",
-                "C" : "i",
-                "D" : "u",
-                "E" : "y",
-                "F" : "k"}
-
+        self.encodeDict = json.load(open("encodeDict.json"))
         self.decodeDict = {v: k for k, v in self.encodeDict.items()}
 
     def encoder(self):
-        real = input("Enter text to be encoded: ")
-        real = real.upper()
-        fake = ""
-        for letter in real:
-            fake = fake + self.encodeDict[letter]
+        try:
+            real = input("Enter text to be encoded: ")
+            real = real.upper()
+            fake = ""
+            for letter in real:
+                fake = fake + self.encodeDict[letter]
 
-        print(fake)
+            print(fake)
+
+        except Exception as Err:
+            print("Error in Encoder: ", Err)
+
 
     def decoder(self):
-        farzi = input("Enter text to be decoded: ")
-        farzi = farzi.lower()
-        asli = ""
-        for chotu in farzi:
-            asli = asli + self.decodeDict[chotu]
+        try:
+            farzi = input("Enter text to be decoded: ")
+            farzi = farzi.lower()
+            asli = ""
+            for chotu in farzi:
+                asli = asli + self.decodeDict[chotu]
 
-        print(asli)
+            print(asli)
+        
+        except Exception as Err:
+            print("Error in Decoder: ", Err)
 
 
 def main():
-    ENDE = EncoderDecoder()
-    cnt = 0
-    while True:
-        task = input("Enter 1 if you want to Encode, 2 for Decode: ")
-        if task == "1":
-            ENDE.encoder()
-            break
-        elif task == "2":
-            ENDE.decoder()
-            break
-        else:
-            print("Try Again")
-            cnt = cnt + 1
-            if cnt > 2:
-                break  
-        
-    print("Bhak")
+    try:
+        ENDE = EncoderDecoder()
+        cnt = 0
+        while True:
+            task = input("Enter 1 if you want to Encode, 2 for Decode: ")
+            if task == "1":
+                ENDE.encoder()
+                break
+            elif task == "2":
+                ENDE.decoder()
+                break
+            else:
+                print("Try Again")
+                cnt = cnt + 1
+                if cnt > 2:
+                    break  
+            
+        print("Bhak")
+
+    except Exception as BigErr:
+        print("Tut gaya thoda main: ", BigErr)
 
 if __name__ == "__main__":
     main()
